7fe10bc40cda4fe55858b9a45167e3a7dea0e874
No name <p.n.thang0101@gmail.com>
2023-01-10 10:23:19 +0700

    10/01/2023:

---
+++ ai_radio/android/app/src/main/java/com/example/ai_radio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ai_radio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ai_radio/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'home_page.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
